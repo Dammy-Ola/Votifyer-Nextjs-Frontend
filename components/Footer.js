69f0816d2eaf58adeb509d67c43b4f16b482fbec
Dammy-Ola <dammyolapaix@gmail.com>
2021-07-23 10:55:43 +0000

    Fixed external link to techbiznez

--- components/Footer.js
+++ components/Footer.js
@@ -1,14 +1,13 @@
+import Link from 'next/link'
 export default function Footer() {
   return (
     <div className='my-10 text-center font-bold'>
       Votifyer From{' '}
-      <a
-        href='https:techbiznez.com/'
-        target='__blank'
-        className='text-secondary'
-      >
-        TechBiznez
-      </a>
+      <Link href='https:techbiznez.com/' passHref={true}>
+        <a target='__blank' className='text-secondary'>
+          TechBiznez
+        </a>
+      </Link>
     </div>
   )
 }
